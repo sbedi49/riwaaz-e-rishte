04d8ac6c169da8cf9e5da4061a4e64c32fe62e13
Sarab Bedi <sarabsbedi@gmail.com>
2021-01-27 19:12:25 -0500

    Added Responsive Mobile Menu to Index Page

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const toggle = document.getElementById("toggle")
-const main = document.getElementById("main")
+const toggle = document.getElementById("toggle");
+const respNav = document.getElementById("respNav");
+
+const barOne = document.getElementsByClassName("bar one")[0];
+const barTwo = document.getElementsByClassName("bar two")[0];
+const barThree = document.getElementsByClassName("bar three")[0];
 
 toggle.addEventListener("click",()=>{
-    main.classList.toggle("active")
+    respNav.classList.toggle("active");
+
+    barOne.classList.toggle("active");
+    barTwo.classList.toggle("active");
+    barThree.classList.toggle("active");
+
+
 })
 
